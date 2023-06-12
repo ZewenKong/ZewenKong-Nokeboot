37443162194c3f492a60b2879fdf23cc338717c4
ZewenKong <zewen.kong@outlook.com>
2023-06-12 16:48:04 +0100

    C

---
+++ [Programming] C/4-[Intro]-Selection-&-Loop-statements.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+// 4. Selection & Loop statments
+
+// 4.1 Selection statment
+
+// 1) If..else
+
+// Example
+// - - - - - - - - - -
+//int main(void)
+//{
+//    int input = 0;
+//    printf("Study? (0/1)\n");
+//
+//    scanf("%d", &input);
+//    if (input == 1)  // (input = 1) is true, run the if block
+//    {
+//        printf("Good\n");
+//    }
+//    else  // else run the else block
+//    {
+//        printf("Lazy\n");
+//    }
+//
+//    return 0;
+//}
+// - - - - - - - - - -
+
+// 2) Switch statment
+// ...
+
+// 4.2 Loop statment
+
+// 1) while loop
+
+// Example
+// - - - - - - - - - -
+//int main(void)
+//{
+//    int codeline = 0;
+//    printf("Join Google/Microsoft\n");
+//
+//    while (codeline < 20000)
+//    {
+//        printf("Coding: %d\n", codeline);
+//        codeline ++;
+//    }
+//
+//    if (codeline >= 20000)
+//    {
+//        printf("Congrats\n");
+//    }
+//    else
+//    {
+//        printf("Continue\n");
+//    }
+//
+//    return 0;
+//}
+// - - - - - - - - - -
+
+// 2) for loop
+// ...
+
+// 3) do...while loop
+// ...
---
+++ [Programming] C/5-[Intro]-Functions-&-Arrays.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+// 5. Functions & Arrays
+
+// 5.1 Functions
+
+// Example
+// - - - - - - - - - -
+
+// Function "Add"
+
+// Function name, Add
+// Function arguments, int x, int y
+// Return type, the "int" befor "Add"
+//int Add(int x, int y)
+//{
+//    // Function body
+//    int z = x + y;
+//    return z;
+//}
+
+// Simplify the Function "Add"
+//int Add(int x, int y)
+//{
+//    return (x + y);
+//}
+
+// Main function
+//int main(void)
+//{
+//    int n1 = 0;
+//    int n2 = 0;
+//    int sum = 0;
+//
+//    // Inputs
+//    scanf("%d%d", &n1, &n2);
+//    // Sum up
+//    sum = Add(n1, n2);
+//    // Print
+//    printf("%d\n", sum);
+//
+//    return 0;
+//}
+// - - - - - - - - - -
+
+// 5.2 Arrays
+// Defi. of Arrays, 一组相同类型元素的集合
+
+// Example
+// - - - - - - - - - -
+int main(void)
+{
+    // Arrays
+    // int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // OR
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    // 数组下标
+    // 下标为数组内元素的序号 - 从零开始
+    
+    // 打印数组内的第九个元素
+    printf("%d\n", arr[8]);
+    printf("");
+     
+    // 打印数组内所有元素
+    int i = 0;
+    while (i < 10)
+    {
+        printf("%d\n", arr[i]);
+        i ++;
+    }
+    
+    return 0;
+}
+// - - - - - - - - - -
