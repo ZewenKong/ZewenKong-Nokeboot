806ff7d176ad5416d42a75726b89d0c970feec6a
ZewenKong <zewen.kong@outlook.com>
2024-01-01 17:14:45 +0000

    20240101

---
+++ 03 - Programming/04 - Python/Lecture01.py
@@ -0,0 +1,5 @@
+# 00 Hello World!
+
+print("Hello World!")
+
+# 01 Variables and assignments
--- 03 - Programming/99 - Daileetcode/001 - Two Sum.py
+++ 03 - Programming/99 - Daileetcode/001 - Two Sum.py
@@ -1,7 +1,7 @@
 # Method 1: Loop though the whole list
 def two_sum(nList, tValue):
     dic = {}
-    for i in range(len(nlist)):  # loop through number in the list
+    for i in range(len(nList)):  # loop through number in the list
         if tValue - nList[i] in dic:  # target minus the number
             return [dic[tValue - nList[i]], i]
             # return the place of the value (first argument), and the place of current number
