d0bbc63a6cd9f24e8229c092a6578ec2ecdf39df
ZewenKong <zewen.kong@outlook.com>
2024-01-02 14:43:43 +0000

    240102

--- 03 - Programming/99 - Daileetcode/009 - Palindrome Number [E].py
+++ 03 - Programming/99 - Daileetcode/009 - Palindrome Number [E].py
@@ -1,2 +1,11 @@
-# Easy
+# Palindrome Number 回文数
+def isPalindrome(num):
+    return str(num) == str(num)[::-1]
 
+
+# str(num) 将整数 num 转换成字符串
+# str(num)[::-1] 逆字符串
+
+print(isPalindrome(121))
+
+print(isPalindrome(123))
