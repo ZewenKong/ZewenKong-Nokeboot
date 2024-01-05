112fd1c1fb3996387b6a199c7e98f5ec1dd5a25e
ZewenKong <zewen.kong@outlook.com>
2024-01-05 16:36:51 +0000

    240105

---
+++ 03 - Programming/99 - Leetcode/020 - Valid Parentheses [E].py
@@ -0,0 +1,13 @@
+def validParentheses(s):
+
+    while len(s) > 0:
+        l = len(s)
+        s = s.replace('()', '').replace('{}', '').replace('[]', '')
+
+        if l == len(s):
+            return False
+
+    return True
+
+print(validParentheses("{}"))
+print(validParentheses("{]"))
