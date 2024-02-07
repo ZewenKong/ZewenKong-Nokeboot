0966c1ec25fae0864efb959f4e653030bbc27127
ZewenKong <zewen.kong@outlook.com>
2024-02-07 01:20:13 +0000

    240207

---
+++ 03 - Programming/99 - Leetcode/0709 - To Lower Case [E].py
@@ -0,0 +1,10 @@
+def toLowerCase(s):
+
+    # lis = list(s)
+    # for i in range(len(lis)):
+    #     c = ord(lis[i])
+    #     if 65 <= c <= 90:
+    #         lis[i] = chr(c + 32)
+    # return ''.join(lis)
+
+    return s.lower()
