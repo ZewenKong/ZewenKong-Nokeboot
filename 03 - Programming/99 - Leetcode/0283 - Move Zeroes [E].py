c7d768d416ab386d071b62efb321655502e8f764
ZewenKong <zewen.kong@outlook.com>
2024-02-29 10:57:20 +0000

    240229

---
+++ 03 - Programming/99 - Leetcode/0283 - Move Zeroes [E].py
@@ -0,0 +1,12 @@
+def moveZeroes(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    i = 0
+    for j in range(n):
+        if (nums[j] != 0):
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            
