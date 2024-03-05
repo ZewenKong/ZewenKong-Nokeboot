3fe65fc7406e03a355b8cbb527642bd5f7225f84
ZewenKong <zewen.kong@outlook.com>
2024-03-05 14:19:33 +0000

    240305

---
+++ 03 - Programming/99 - Leetcode/0796 - Rotate String [E].py
@@ -0,0 +1,15 @@
+def rotateString(s, goal):
+    """
+    :type s: str
+    :type goal: str
+    :rtype: bool
+    """
+    if len(s) != len(goal):
+        return False
+
+    if len(s) > 0 and len(goal) > 0:
+        new_s = s + s
+        return (goal in new_s)
+    else:
+        return False
+    
