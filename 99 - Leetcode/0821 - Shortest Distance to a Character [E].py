6c36015e59de32f2a3bfa8a5dc94370c3b6023fc
ZewenKong <zewen.kong@outlook.com>
2024-03-12 15:38:48 +0000

    240312

---
+++ 99 - Leetcode/0821 - Shortest Distance to a Character [E].py
@@ -0,0 +1,13 @@
+def shortestToChar(s, c):
+    """
+    :type s: str
+    :type c: str
+    :rtype: List[int]
+    """
+    lengthOf_string = len(s)
+    result = [lengthOf_string] * lengthOf_string
+    lastItem_string = -lengthOf_string
+    for i in list(range(lengthOf_string)) + list(range(lengthOf_string)[::-1]):
+        if (s[i] == c):  lastItem_string = i
+        result[i] = min(result[i], abs(i - lastItem_string))
+    return result
